c520a6f8bda7ca8373d993c009994cc869eb028a
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2012-05-28 16:40:19 +0200

    Added english language to sample contents

--- files/acp/install_sample-contents.php
+++ files/acp/install_sample-contents.php
@@ -4,6 +4,25 @@
  * @copyright	2009-2012 WCF Solutions <http://www.wcfsolutions.com/>
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
+/**
+ * Compiles the given string.
+ *
+ * @param	string		$string
+ * @return	string
+ */
+function compileTPL($string) {
+	if (strpos($string, '{') !== false) {
+		require_once(WCF_DIR.'lib/system/template/TemplateScriptingCompiler.class.php');
+		$scriptingCompiler = new TemplateScriptingCompiler(WCF::getTPL());
+		try {
+			return $scriptingCompiler->compileString('moxeoInstallation', $string);
+		}
+		catch (SystemException $e) {}
+	}
+	return $string;
+}
+
+// vars
 $packageID = $this->installation->getPackageID();
 $parentPackageID = $this->installation->getPackage()->getParentPackageID();
 $instanceNo = $this->installation->getPackage()->getParentPackage()->getInstanceNo();
@@ -21,33 +40,68 @@ $theme = ThemeEditor::import($sourceFile, $packageID);
 
 // create default contents
 if ($theme->themeID) {
+	// language items
+	$languages = array(
+		'de' = array(
+			'contentItem.index' => 'Startseite',
+			'contentItem.index.alias' => 'startseite',
+			'contentItem.index.welcome' => 'Willkommen bei Moxeo Open Source CMS!',
+			'contentItem.index.welcome.description' => '<p>Gl&uuml;ckwunsch, Sie haben <em>Moxeo Open Source CMS</em> erfolgreich installiert! Das war einfach, oder? Sie k&ouml;nnen nun &uuml;ber die <a href="./acp/">Administrationsoberfl&auml;che</a> neue Inhalte hinzuf&uuml;gen.</p>',
+			'copyright' => '<a href="http://www.moxeo.org/">Software: <strong>Moxeo Open Source CMS</strong>, entwickelt von WCF Solutions</a>',
+			'theme.default' => 'Standard',
+			'theme.module.userPanel' => 'Benutzerpanel',
+			'theme.module.pageTitle' => 'Seitentitel',
+			'theme.module.mainMenu' => 'Hauptmenü',
+			'theme.module.subMenu' => 'Untermenü',
+			'theme.module.breadCrumbs' => 'Brotkrümel-Navigation',
+			'theme.module.article' => 'Artikel',
+			'theme.module.footer' => 'Footer',
+			'userNote' => '{if $this->user->userID}Angemeldet als {$this->user->username}.{else}Sie sind nicht angemeldet.{/if}'
+		),
+		'en' = array(
+			'contentItem.index' => 'Index page',
+			'contentItem.index.alias' => 'index',
+			'contentItem.index.welcome' => 'Welcome to Moxeo Open Source CMS!',
+			'contentItem.index.welcome.description' => '<p>Congratulations, you have installed <em>Moxeo Open Source CMS</em> successfully! That was easy, wasn\'t it? You can now add new contents using the <a href="./acp/">Administration Control Panel</a></p>',
+			'copyright' => '<a href="http://www.moxeo.org/">Software: <strong>Moxeo Open Source CMS</strong>, developed by WCF Solutions</a>',
+			'theme.default' => 'Default',
+			'theme.module.userPanel' => 'User Panel',
+			'theme.module.pageTitle' => 'Page Title',
+			'theme.module.mainMenu' => 'Main Menu',
+			'theme.module.subMenu' => 'Sub Menu',
+			'theme.module.breadCrumbs' => 'Bread Crumbs',
+			'theme.module.article' => 'Article',
+			'theme.module.footer' => 'Footer',
+			'userNote' => '{if $this->user->userID}Welcome {$this->user->username}.{else}You are not logged in.{/if}'
+		)
+	);
+
+	// map language items
+	$languageCode = WCF::getLannguage()->getLanguageCode();
+	$language = (isset($languages[$languageCode]) ? $languages[$languageCode] : $languages['en']);
+
 	// create theme layout
 	require_once(WCF_DIR.'lib/data/theme/layout/ThemeLayoutEditor.class.php');
-	$themeLayout = ThemeLayoutEditor::create($theme->themeID, 'Default', "global\nlayout\nnavigation\nfile\nimage\nnews\ncomment\nform", $packageID);
+	$themeLayout = ThemeLayoutEditor::create($theme->themeID, $language['theme.default'], "global\nlayout\nnavigation\nfile\nimage\nnews\ncomment\nform", $packageID);
 	$themeLayout->setAsDefault($parentPackageID);
 
 	// create default modules
 	require_once(WCF_DIR.'lib/data/theme/module/ThemeModuleEditor.class.php');
-	$userPanelThemeModule = ThemeModuleEditor::create($theme->themeID, 'Page Title', 'userPanel', '', 'html', array(
-		'code' => '<div id="userNote">{if $this->user->userID}Angemeldet als {$this->user->username}.{else}Sie sind nicht angemeldet.{/if}</div>',
-		'dynamicCode' => '<div id="userNote"><?php if ($this->v[\'this\']->user->userID) { ?>Angemeldet als <?php echo StringUtil::encodeHTML($this->v[\'this\']->user->username); ?>.<?php } else { ?>Sie sind nicht angemeldet.<?php } ?></div>'
+	$userPanelThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.userPanel'], 'userPanel', '', 'html', array(
+		'code' => '<div id="userNote">'.$language['userNote'].'</div>',
+		'dynamicCode' => compileTPL('<div id="userNote">'.$language['userNote'].'</div>')
 	), $packageID);
-	$headerThemeModule = ThemeModuleEditor::create($theme->themeID, 'Page Title', 'pageTitle', '', 'html', array(
+	$headerThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.pageTitle'], 'pageTitle', '', 'html', array(
 		'code' => '<a href="./">{PAGE_TITLE}</a>',
-		'dynamicCode' => '<a href="./"><?php echo StringUtil::encodeHTML(PAGE_TITLE); ?></a>'
+		'dynamicCode' => compileTPL('<a href="./">{PAGE_TITLE}</a>')
 	), $packageID);
-	$mainNavigationThemeModule = ThemeModuleEditor::create($theme->themeID, 'Main Menu', 'mainMenu', '', 'navigation', array('levelOffset' => 0, 'levelLimit' => 1), $packageID);
-	$subNavigationThemeModule = ThemeModuleEditor::create($theme->themeID, 'Sub Menu', 'subMenu', '', 'navigation', array('levelOffset' => 1, 'levelLimit' => 5), $packageID);
-	$breadCrumbThemeModule = ThemeModuleEditor::create($theme->themeID, 'Bread Crumbs', '', '', 'breadCrumb', array(), $packageID);
-	$articleThemeModule = ThemeModuleEditor::create($theme->themeID, 'Article', '', '', 'article', array(), $packageID);
-	$footerThemeModule = ThemeModuleEditor::create($theme->themeID, 'Footer', '', '', 'html', array(
-		'code' => '<a href="http://www.moxeo.org/">Software: <strong>Moxeo Open Source CMS</strong>, entwickelt von WCF Solutions</a> | {@TIME_NOW|fulldate}',
-		'dynamicCode' => '<?php
-if (!isset($this->pluginObjects[\'TemplatePluginModifierFulldate\'])) {
-require_once(WCF_DIR.\'lib/system/template/plugin/TemplatePluginModifierFulldate.class.php\');
-$this->pluginObjects[\'TemplatePluginModifierFulldate\'] = new TemplatePluginModifierFulldate;
-}
-?><a href="http://www.moxeo.org/">Software: <strong>Moxeo Open Source CMS</strong>, entwickelt von WCF Solutions</a> | <?php echo $this->pluginObjects[\'TemplatePluginModifierFulldate\']->execute(array(TIME_NOW), $this); ?>'
+	$mainNavigationThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.mainMenu'], 'mainMenu', '', 'navigation', array('levelOffset' => 0, 'levelLimit' => 1), $packageID);
+	$subNavigationThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.subMenu'], 'subMenu', '', 'navigation', array('levelOffset' => 1, 'levelLimit' => 5), $packageID);
+	$breadCrumbThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.breadCrumbs'], '', '', 'breadCrumb', array(), $packageID);
+	$articleThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.article'], '', '', 'article', array(), $packageID);
+	$footerThemeModule = ThemeModuleEditor::create($theme->themeID, $language['theme.module.footer'], '', '', 'html', array(
+		'code' => $language['copyright'].' | {@TIME_NOW|fulldate}',
+		'dynamicCode' => compileTPL($language['copyright'].' | {@TIME_NOW|fulldate}')
 	), $packageID);
 
 	// add modules to layout
@@ -62,22 +116,22 @@ $this->pluginObjects[\'TemplatePluginModifierFulldate\'] = new TemplatePluginMod
 	// create index page
 	$sql = "INSERT INTO	moxeo".WCF_N."_".$instanceNo."_content_item
 				(languageID, parentID, title, contentItemAlias)
-		VALUES		(".WCF::getLanguage()->getLanguageID().", 0, 'Startseite', 'startseite')";
+		VALUES		(".WCF::getLanguage()->getLanguageID().", 0, '".escapeString($language['contentItem.index'])."', '".escapeString($language['contentItem.index.alias'])."')";
 	WCF::getDB()->sendQuery($sql);
 	$contentItemID = WCF::getDB()->getInsertID("moxeo".WCF_N."_".$instanceNo."_content_item", 'contentItemID');
 
 	// create article
 	$sql = "INSERT INTO	moxeo".WCF_N."_".$instanceNo."_article
 				(contentItemID, themeModulePosition, title)
-		VALUES		(".$contentItemID.", 'main', 'Startseite')";
+		VALUES		(".$contentItemID.", 'main', '".escapeString($language['contentItem.index'])."')";
 	WCF::getDB()->sendQuery($sql);
 	$articleID = WCF::getDB()->getInsertID("moxeo".WCF_N."_".$instanceNo."_article", 'articleID');
 
 	// save article section
 	$articleSectionData = array(
-		'headline' => 'Willkommen bei Moxeo Open Source CMS!',
+		'headline' => $language['contentItem.index.welcome'],
 		'headlineSize' => 1,
-		'code' => '<p>Gl&uuml;ckwunsch, Sie haben <em>Moxeo Open Source CMS</em> erfolgreich installiert! Das war einfach, oder? Sie k&ouml;nnen nun &uuml;ber die <a href="./acp/">Administrationsoberfl&auml;che</a> neue Inhalte hinzuf&uuml;gen.</p>',
+		'code' => $language['contentItem.index.welcome.description'],
 		'thumbnail' => '',
 		'thumbnailCaption' => '',
 		'thumbnailAlternativeTitle' => '',
